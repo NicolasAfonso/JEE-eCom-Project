2752bee5f858d5ed3db4769ab0c2dacc09c3782f
NicolasAfonso <afonso.nicolas@live.fr>
2013-11-12 18:30:50 +0100

    Product dao correction

--- stlagora/src/main/java/com/stlagora/model/dao/ProductDaoImpl.java
+++ stlagora/src/main/java/com/stlagora/model/dao/ProductDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Query;
 
+import com.stlagora.model.entities.Category;
 import com.stlagora.model.entities.Product;
 import com.stlagora.model.entities.User;
 
@@ -19,7 +20,7 @@ public class ProductDaoImpl extends GenericDAOImpl<Product> implements ProductDa
 		super();
 	}
 	public Product findByName(String name) {
-		Query q = em.createQuery("SELECT p FROM User p WHERE p.name=:name",Product.class);
+		Query q = em.createQuery("SELECT p FROM PRODUCTS p WHERE p.name=:name",Product.class);
 		q.setParameter("name",name);
 		List<Product> products = q.getResultList();
 		if(products.isEmpty())
@@ -34,35 +35,35 @@ public class ProductDaoImpl extends GenericDAOImpl<Product> implements ProductDa
 	}
 
 	public List<Product> findByPrice(Float price) {
-		Query q = em.createQuery("SELECT p FROM User p WHERE p.price=:price",Product.class);
+		Query q = em.createQuery("SELECT p FROM PRODUCTS p WHERE p.price=:price",Product.class);
 		q.setParameter("price",price);
 		List<Product> products = q.getResultList();
 		return (List<Product>) products.get(0);
 	}
 
 	public List<Product> findBySeller(User seller) {
-		Query q = em.createQuery("SELECT p FROM User p WHERE p.seller_id=:seller_id",Product.class);
+		Query q = em.createQuery("SELECT p FROM PRODUCTS p WHERE p.seller_id=:seller_id",Product.class);
 		q.setParameter("seller_id",seller.getId());
 		List<Product> products = q.getResultList();
 		return products;
 	}
 
 	public List<Product> findByAvailableDate(Date availableDate) {
-		Query q = em.createQuery("SELECT p FROM User p WHERE p.availableDate=:availableDate",Product.class);
+		Query q = em.createQuery("SELECT p FROM PRODUCTS p WHERE p.availableDate=:availableDate",Product.class);
 		q.setParameter("availableDate",availableDate);
 		List<Product> products = q.getResultList();
 		return products;
 	}
 
 	public List<Product> findByLastUpdate(Date lastUpdate) {
-		Query q = em.createQuery("SELECT p FROM User p WHERE p.lastUpdate=:lastUpdate",Product.class);
+		Query q = em.createQuery("SELECT p FROM PRODUCTS p WHERE p.lastUpdate=:lastUpdate",Product.class);
 		q.setParameter("availableDate",lastUpdate);
 		List<Product> products = q.getResultList();
 		return products;
 	}
 
 	public List<Product> findByGlobalMark(Float globalMark) {
-		Query q = em.createQuery("SELECT p FROM User p WHERE p.globalMark=:globalMark",Product.class);
+		Query q = em.createQuery("SELECT p FROM PRODUCTS p WHERE p.globalMark=:globalMark",Product.class);
 		q.setParameter("availableDate",globalMark);
 		List<Product> products = q.getResultList();
 		return products;
