ead913c31fcd8013c70f722bdaf2dc273bb573be
NicolasAfonso <afonso.nicolas@live.fr>
2013-11-12 18:31:18 +0100

    Userdao and UserdaoImpl

---
+++ stlagora/src/main/java/com/stlagora/model/dao/UserDAO.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.stlagora.model.dao;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.stlagora.model.entities.User;
+import com.stlagora.model.entities.enumerate.ROLE;
+
+public interface UserDAO extends GenericDAO<User>  {
+
+	
+	public User findByPseudo(String pseudo);
+	public List<User> findBySurname(String surname);
+	public List<User> findByFirstname(String firstname);
+	public User findByEmail(String email);
+	public List<User> findBySubscriptionDate(Date subscriptionDate);
+	public List<User> findByPhoneNumber(String phoneNumber);
+	public List<User> findByRole(ROLE role);
+}
