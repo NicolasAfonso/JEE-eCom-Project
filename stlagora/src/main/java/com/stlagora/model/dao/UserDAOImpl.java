88de84164350504202b63e27a1e7783d98ea3d3d
Bianco Jean-François <bianco.jfrancois@gmail.com>
2013-11-12 21:37:53 +0100

    Correction UserDAOImpl -> Erreur venait du nom qui n'était pas identique dans les requêtes que dans la classe

--- stlagora/src/main/java/com/stlagora/model/dao/UserDAOImpl.java
+++ stlagora/src/main/java/com/stlagora/model/dao/UserDAOImpl.java
@@ -25,49 +25,48 @@ public class UserDAOImpl extends GenericDAOImpl< User > implements UserDAO{
 	}
 	
 	public User findByPseudo(String pseudo) {
-		Query q = em.createQuery("SELECT u FROM USERS WHERE u.pseudo =:pseudo");
+		Query q = em.createQuery("SELECT u FROM User u WHERE u.pseudo =:pseudo");
 		q.setParameter("pseudo", pseudo);
 		List<User> results = q.getResultList();
 		return results.get(0);
 	}
 
 	public List<User> findBySurname(String surname) {
-		Query q = em.createQuery("SELECT u FROM USERS WHERE u.surname =:surname");
+		Query q = em.createQuery("SELECT u FROM User u WHERE u.surname =:surname");
 		q.setParameter("surname", surname);
 		List<User> results = q.getResultList();
 		return results;
 	}
 
 	public List<User> findByFirstname(String firstname) {
-		Query q = em.createQuery("SELECT u FROM USERS WHERE u.firstname =:firstname");
+		Query q = em.createQuery("SELECT u FROM User u WHERE u.firstname =:firstname");
 		q.setParameter("firstname", firstname);
 		List<User> results = q.getResultList();
 		return results;
 	}
 
 	public User findByEmail(String email) {
-		Query q = em.createQuery("SELECT u FROM USERS WHERE u.email =:email");
+		Query q = em.createQuery("SELECT u FROM User u WHERE u.email =:email",User.class);
 		q.setParameter("email", email);
-		List<User> results = q.getResultList();
-		return results.get(0);
+		return (User) q.getSingleResult();
 	}
 
 	public List<User> findBySubscriptionDate(Date subscriptionDate) {
-		Query q = em.createQuery("SELECT u FROM USERS WHERE u.subscriptionDate =:subscriptionDate");
+		Query q = em.createQuery("SELECT u FROM User u WHERE u.subscriptionDate =:subscriptionDate");
 		q.setParameter("subscriptionDate", subscriptionDate);
 		List<User> results = q.getResultList();
 		return results;
 	}
 
 	public List<User> findByPhoneNumber(String phoneNumber) {
-		Query q = em.createQuery("SELECT u FROM USERS WHERE u.phoneNumber =:phoneNumber");
+		Query q = em.createQuery("SELECT u FROM User u WHERE u.phoneNumber =:phoneNumber");
 		q.setParameter("phoneNumber", phoneNumber);
 		List<User> results = q.getResultList();
 		return results;
 	}
 
 	public List<User> findByRole(ROLE role) {
-		Query q = em.createQuery("SELECT u FROM USERS WHERE u.role =:role");
+		Query q = em.createQuery("SELECT u FROM User u WHERE u.role =:role");
 		q.setParameter("role", role);
 		List<User> results = q.getResultList();
 		return results;
