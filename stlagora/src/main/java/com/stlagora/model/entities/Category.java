e42d0ab4834a74897b93f00d5506a9635f829207
Bianco Jean-François <bianco.jfrancois@gmail.com>
2013-11-12 11:06:24 +0100

    Ajoute Category au modele

---
+++ stlagora/src/main/java/com/stlagora/model/entities/Category.java
@@ -0,0 +1,72 @@
+package com.stlagora.model.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categories")
+public class Category implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name="id" ,unique=true, nullable=false)
+	private long id;
+
+	@Column(name = "categoryName", nullable = false, unique=true) 
+	private String categoryName;
+
+	@Column (name="categoryDescription" ,nullable=false)
+	private String categoryDescription;
+	
+	@ElementCollection
+	private List<Product> products = new ArrayList<Product>();
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return categoryDescription;
+	}
+
+	public void setDescription(String description) {
+		this.categoryDescription = description;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	} 
+	
+}
