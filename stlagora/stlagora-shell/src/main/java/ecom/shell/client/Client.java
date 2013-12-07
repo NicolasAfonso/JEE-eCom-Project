be0ba19cae7cbdf8dce6deb69f22fe08f0073e7d
Nicolas <Nicolas@Nicolas-XPS>
2013-12-07 17:26:49 +0100

    ajout client dans ecom shell

---
+++ stlagora/stlagora-shell/src/main/java/ecom/shell/client/Client.java
@@ -0,0 +1,14 @@
+package ecom.shell.client;
+
+public class Client {
+	
+	private static void UserLog(){
+		System.out.println("");
+	}
+
+	public static void main(String[] args) {
+		UserLog();
+		
+	}
+	
+}
