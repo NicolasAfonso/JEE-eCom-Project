0ab68da13076c864a52187841ac51454ad98c2f9
Elisa <Elisa@Elisa-PC>
2013-12-10 21:18:31 +0100

    Le layout de la page d'accueil MARCHEEEEEEEEUH !!! EN-FIN ! + menu marche + caroussel images

--- stlagora/stlagora-webapp/src/main/webapp/resources/js/agile_carousel_data.php
+++ stlagora/stlagora-webapp/src/main/webapp/resources/js/agile_carousel_data.php
@@ -1,16 +1,10 @@
 [{
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/paiementSecu.png.xhtml?ln=images' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/caroussel1.png.xhtml?ln=images' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
+      "content_button": "<div class='thumb'><img src='#{resource['images/i1.jpg']}' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/caroussel2.png.xhtml?ln=images' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
       "content_button": "<div class='thumb'><img src='#{resource['images/img1.jpg']}' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
 }, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/paiementSecu.png.xhtml?ln=images' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
+       "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/caroussel3.png.xhtml?ln=images' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
       "content_button": "<div class='thumb'><img src='#{resource['images/img1.jpg']}' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
-}, {
-       "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/paiementSecu.png.xhtml?ln=images' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='#{resource['images/img1.jpg']}' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
-}, {
-       "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/paiementSecu.png.xhtml?ln=images' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='#{resource['images/img1.jpg']}' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
-}, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='/stlagora/javax.faces.resource/img1.jpg.xhtml?ln=images' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
 }]
